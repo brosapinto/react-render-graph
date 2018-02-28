0e547faf11cc0d0579a5ab014b038b2351cdf84a
Bruno Sotto-Mayor Pinto <bruno.sotto-mayor@talkdesk.com>
2018-02-28 15:30:03 +0000

    Creating simple graph using React JSON Graph and Dagre

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Graph from "react-json-graph";
+import dagre from "dagre";
 
 class App extends Component {
+  nodes = [];
+  edges = [];
+
   render() {
+    const graph = new dagre.graphlib.Graph();
+
+    graph.setGraph({}).setDefaultEdgeLabel(() => ({}));
+
+    graph
+      .setNode(0, { label: "Alice", width: 160, height: 36 })
+      .setNode(1, { label: "Bob", width: 160, height: 36 })
+      .setNode(2, { label: "Luke", width: 160, height: 36 })
+      .setNode(3, { label: "John", width: 160, height: 36 });
+
+    graph
+      .setEdge(0, 1)
+      .setEdge(1, 2)
+      .setEdge(1, 1)
+      .setEdge(2, 0)
+      .setEdge(0, 3);
+
+    dagre.layout(graph);
+    this.nodes = graph.nodes().map(id => {
+      const node = graph.node(id);
+      return {
+        id,
+        label: node.label,
+        position: { x: node.x, y: node.y }
+      };
+    });
+
+    this.edges = graph.edges().map(({ v: source, w: target }) => ({
+      source,
+      target
+    }));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Graph
+          width={600}
+          height={600}
+          json={{
+            nodes: this.nodes,
+            edges: this.edges,
+            isStatic: true,
+            isVertical: true,
+            isDirected: true
+          }}
+          onChange={newGraphJSON => {}}
+        />
       </div>
     );
   }
