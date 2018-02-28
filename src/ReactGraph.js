11587755125344244dff71630766ba134bfe277d
Bruno Sotto-Mayor Pinto <bruno.sotto-mayor@talkdesk.com>
2018-02-28 15:30:03 +0000

    Integrating React JSON Graph

--- src/ReactGraph.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ReactGraph = () => <h1>React Graph</h1>;
-
-export default ReactGraph;
---
+++ src/ReactGraph.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Graph from "react-json-graph";
+import dagre from "dagre";
+
+class ReactGraph extends Component {
+  nodes = [];
+  edges = [];
+
+  render() {
+    const graph = new dagre.graphlib.Graph();
+
+    graph.setGraph({}).setDefaultEdgeLabel(() => ({}));
+
+    graph
+      .setNode("A", { label: "A", width: 160, height: 36 })
+      .setNode("C", { label: "C", width: 160, height: 36 })
+      .setNode("B", { label: "B", width: 160, height: 36 })
+      .setNode("D", { label: "D", width: 160, height: 36 })
+      .setNode("E", { label: "E", width: 160, height: 36 })
+      .setNode("F", { label: "F", width: 160, height: 36 })
+      .setNode("G", { label: "G", width: 160, height: 36 })
+      .setNode("H", { label: "H", width: 160, height: 36 });
+
+    graph
+      .setEdge("A", "B")
+      .setEdge("A", "C")
+      .setEdge("B", "G")
+      .setEdge("B", "H")
+      .setEdge("C", "D")
+      .setEdge("C", "E")
+      .setEdge("E", "F")
+      .setEdge("E", "A");
+
+    dagre.layout(graph);
+
+    this.nodes = graph.nodes().map(id => {
+      const node = graph.node(id);
+      return {
+        id,
+        label: node.label,
+        position: { x: node.x, y: node.y }
+      };
+    });
+
+    this.edges = graph.edges().map(({ v: source, w: target }) => ({
+      source,
+      target
+    }));
+
+    return (
+      <React.Fragment>
+        <Graph
+          width={900}
+          height={600}
+          json={{
+            nodes: this.nodes,
+            edges: this.edges,
+            isStatic: true,
+            isVertical: true,
+            isDirected: true
+          }}
+          onChange={newGraphJSON => {}}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default ReactGraph;
