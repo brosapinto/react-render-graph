718ee9a4d893dd87291d3c19e0768426b23fdbc8
José Marques <jose.marques@talkdesk.com>
2018-03-05 16:49:23 +0000

    Actions added: add, remove, zoom in and out

--- src/Cytoscape.jsx
+++ src/Cytoscape.jsx
@@ -3,9 +3,157 @@ import cytoscape from "cytoscape";
 import graphFactory from "./graph-factory";
 import dagre from "dagre";
 
+function toCytoscapeNode(id, dagreNode, classes) {
+  const { x, y, label, width, height } = dagreNode;
+  return {
+    group: "nodes",
+    // grabbable: false,
+    // locked: true,
+    selected: false, // whether the element is selected (default false)
+    selectable: true, // whether the selection state is mutable (default true)
+
+    // data: { weight: 75 },
+    // data: { id, width: node.width, height: node.height },
+    // data: { id, label: node.label, weight: node.weight || 10 },
+    data: { id, label, width, height },
+    // position: { x: grid(x), y: grid(y) }
+    position: { x, y },
+    classes
+  };
+}
+function toCytoscapeEdge(dagreEdge, classes) {
+  const { v: source, w: target, name, label } = dagreEdge;
+  return {
+    group: "edges",
+    data: {
+      source,
+      target,
+      label: label || name
+    },
+    classes
+  };
+}
+
 class Cytoscape extends Component {
+  state = {
+    counter: 0,
+    graph: null,
+    cy: null
+  };
+
+  constructor() {
+    super();
+    this.state.graph = graphFactory();
+    this.addNode = this.addNode.bind(this);
+    this.removeNode = this.removeNode.bind(this);
+    this.zoomIn = this.zoomIn.bind(this);
+    this.zoomOut = this.zoomOut.bind(this);
+  }
+
+  zoomIn() {
+    const { cy } = this.state;
+    const selectedNodes = cy.$("node.selected");
+    const position =
+      selectedNodes.length > 0 ? selectedNodes[0].position() : undefined;
+
+    cy.zoom({
+      level: cy.zoom() + 0.2,
+      position
+    });
+  }
+
+  zoomOut() {
+    const { cy } = this.state;
+    const selectedNodes = cy.$("node.selected");
+    const position =
+      selectedNodes.length > 0 ? selectedNodes[0].position() : undefined;
+    cy.zoom({
+      level: cy.zoom() - 0.2,
+      position
+    });
+  }
+
+  addNode() {
+    const { graph, cy, counter } = this.state;
+    const nodeId = `e${counter}`;
+    this.setState({ counter: counter + 1 });
+
+    graph.setNode(nodeId, {
+      label: nodeId,
+      width: 100,
+      height: 50
+    });
+    graph.setEdge({
+      v: "icceoch",
+      w: nodeId,
+      name: "new",
+      minlen: 2
+      // weight
+    });
+    graph.setEdge({
+      v: nodeId,
+      w: "ieme",
+      name: "new",
+      minlen: 2
+      // weight
+    });
+
+    // generate the new layout
+    dagre.layout(graph);
+
+    // sync the new layout with cytoscape
+    cy.startBatch();
+    graph.nodes().forEach(id => {
+      const dagreNode = graph.node(id);
+      const cyNode = cy.getElementById(id);
+
+      if (nodeId !== id) {
+        cyNode.position({ x: dagreNode.x, y: dagreNode.y });
+      } else {
+        cy.add(toCytoscapeNode(id, dagreNode, "new"));
+      }
+    });
+    graph
+      .edges()
+      .filter(e => e.v === nodeId || e.w === nodeId)
+      .forEach(edge => cy.add(toCytoscapeEdge(edge, "new")));
+    cy.endBatch();
+  }
+
+  removeNode(evt) {
+    const { graph, cy, counter } = this.state;
+    const cytoscapeNode = evt.target;
+    const nodeId = cytoscapeNode.id();
+
+    // clean Dagre graph
+    graph.edges().forEach(({ v, w }) => {
+      if (v === nodeId || w === nodeId) {
+        graph.removeEdge(v, w);
+      }
+    });
+    graph.removeNode(nodeId);
+
+    // generate the new layout
+    dagre.layout(graph);
+
+    // sync the new layout with cytoscape
+    cy.startBatch();
+    cytoscapeNode.remove();
+    graph.nodes().forEach(id => {
+      const { x, y } = graph.node(id);
+      const cyNode = cy.getElementById(id);
+
+      cyNode.position({ x, y });
+    });
+    cy.endBatch();
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
   componentDidMount() {
-    const graph = graphFactory();
+    const { graph } = this.state;
     dagre.layout(graph);
 
     const cy = cytoscape({
@@ -17,6 +165,7 @@ class Cytoscape extends Component {
       minZoom: 0.3,
       maxZoom: 2,
 
+      // selector: "node:selected",
       style: [
         {
           selector: "node",
@@ -35,7 +184,12 @@ class Cytoscape extends Component {
           }
         },
         {
-          // selector: "node:selected",
+          selector: "node.new",
+          style: {
+            "border-color": "yellow"
+          }
+        },
+        {
           selector: "node.selected",
           style: {
             "border-style": "double",
@@ -60,7 +214,13 @@ class Cytoscape extends Component {
           }
         },
         {
-          // selector: "node:selected",
+          selector: "edge.new",
+          style: {
+            "line-color": "yellow",
+            "target-arrow-color": "yellow"
+          }
+        },
+        {
           selector: "edge.selected",
           style: {
             "line-color": "green",
@@ -70,47 +230,24 @@ class Cytoscape extends Component {
       ]
     });
 
+    this.setState({ cy });
     // const grid = a => a - a % 80;
 
     graph.nodes().forEach(id => {
-      const { x, y, label, width, height } = graph.node(id);
-      // debugger;
-      cy.add({
-        group: "nodes",
-        // grabbable: false,
-        // locked: true,
-        selected: false, // whether the element is selected (default false)
-        selectable: true, // whether the selection state is mutable (default true)
-
-        // data: { weight: 75 },
-        // data: { id, width: node.width, height: node.height },
-        // data: { id, label: node.label, weight: node.weight || 10 },
-        data: { id, label, width, height },
-        // position: { x: grid(x), y: grid(y) }
-        position: { x, y }
-      });
+      const node = graph.node(id);
+      cy.add(toCytoscapeNode(id, node));
     });
 
-    graph.edges().forEach(edge => {
-      const { v: source, w: target, name, label } = edge;
-      // debugger;
-      cy.add({
-        group: "edges",
-        data: {
-          source,
-          target,
-          label: label || name
-        }
-      });
-    });
+    graph.edges().forEach(edge => cy.add(toCytoscapeEdge(edge)));
 
     const selectedClass = "selected";
     cy.on("click", "node", function(evt) {
       // var node = cy.getElementById(evt.target.id());
-      // console.log("click node " + node.id());
       // const isSelected = node.selected();
       // node.select();
       const node = evt.target;
+      console.log("click node " + node.id());
+
       const isSelected = node.hasClass(selectedClass);
       if (isSelected) {
         node.removeClass(selectedClass);
@@ -121,11 +258,11 @@ class Cytoscape extends Component {
       }
     });
 
+    cy.on("cxttap", "node", this.removeNode);
     // cy.on("cxttap", "node", function(evt) {
     //   var node = evt.target;
     //   console.log("cxttap node " + node.cy());
     // });
-
     // cy.on("click", "edge", function(evt) {
     //   var edge = evt.target;
     //   debugger;
@@ -138,14 +275,29 @@ class Cytoscape extends Component {
     // });
 
     const layout = cy.layout({
-      name: "preset",
-      selectable: true
+      name: "preset"
     });
     layout.run();
   }
 
   render() {
-    return <div style={{ height: "750px", width: "1200px" }} id="canvas" />;
+    return (
+      <React.Fragment>
+        <div className="actions">
+          <a className="action" onClick={this.addNode}>
+            Add Node
+          </a>
+          <span>|</span>
+          <a className="action" onClick={this.zoomIn}>
+            Zoom In
+          </a>
+          <a className="action" onClick={this.zoomOut}>
+            Zoom Out
+          </a>
+        </div>
+        <div style={{ height: "750px", width: "1200px" }} id="canvas" />;
+      </React.Fragment>
+    );
   }
 }
 
