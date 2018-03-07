6b79b7ed0d4c860ebcf6062b679250765db21d86
Bruno Sotto-Mayor Pinto <bruno.sotto-mayor@talkdesk.com>
2018-03-07 19:06:32 +0000

    Re-rendering when adding nodes

--- src/ReactGraph.jsx
+++ src/ReactGraph.jsx
@@ -1,9 +1,63 @@
 import React, { Component } from "react";
-import Graph from "react-json-graph";
+import { default as Graph, Node } from "react-json-graph";
 import dagre from "dagre";
 import graphFactory from "./graph-factory";
 
+class MyGraph extends Graph {
+  renderNode(node) {
+    const { Node: CustomNode, shouldNodeFitContent } = this.props;
+    const { width, height } = node.size || {};
+    const { scale, isStatic } = this.state;
+    const NodeComponent = CustomNode || Node;
+
+    return (
+      <NodeComponent
+        scale={scale}
+        key={`node_${node.id}`}
+        ref={component => {
+          component && this.nodeComponents.push(component);
+        }}
+        getGraph={() => this.graphContainer}
+        onChange={nodeJSON => {
+          this._onChange(nodeJSON);
+        }}
+        x={node.position ? node.position.x : 0}
+        y={node.position ? node.position.y : 0}
+        width={width}
+        height={height}
+        isStatic={isStatic}
+        shouldFitContent={shouldNodeFitContent}
+        {...node}
+      />
+    );
+  }
+}
+
+class MyNode extends Node {
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  render() {
+    const { x, y, width, height } = this.props;
+    const { label } = this.state;
+
+    return (
+      <div
+        style={{ left: x, top: y, position: "absolute", width, height }}
+        ref={element => {
+          this.element = element;
+        }}
+        onMouseDown={event => {}}
+      >
+        {this.renderContainer({ isDragging: false, content: label })}
+      </div>
+    );
+  }
+}
+
 class ReactGraph extends Component {
+  graph = graphFactory();
   state = {
     nodes: [],
     edges: []
@@ -19,9 +73,11 @@ class ReactGraph extends Component {
 
     return nodes
       .map(id => ({ id, ...graph.node(id) }))
-      .map(({ id, label, x, y }) => ({
+      .map(({ id, label, x, y, width, height }) => ({
         id,
         label,
+        width,
+        height,
         position: { x, y }
       }));
   };
@@ -35,13 +91,35 @@ class ReactGraph extends Component {
     edges.map(({ v: source, w: target }) => ({ source, target }));
 
   componentDidMount() {
-    const graph = graphFactory();
-    dagre.layout(graph);
+    dagre.layout(this.graph);
 
     // convert nodes and edges to something
     this.setState({
-      nodes: ReactGraph.convertNodes(graph),
-      edges: ReactGraph.convertEdges(graph.edges())
+      nodes: ReactGraph.convertNodes(this.graph),
+      edges: ReactGraph.convertEdges(this.graph.edges())
+    });
+  }
+
+  addNode() {
+    const randomInt = (min, max) =>
+      Math.floor(Math.random() * (max - min) + min);
+
+    const nodes = this.graph.nodes();
+    const parentNodeId = nodes[randomInt(1, nodes.length)];
+    const randomId = parentNodeId + String.fromCharCode(randomInt(65, 90));
+
+    this.graph
+      .setNode(randomId, {
+        label: randomId,
+        width: 160,
+        height: 36
+      })
+      .setEdge(parentNodeId, randomId);
+    dagre.layout(this.graph);
+
+    this.setState({
+      nodes: ReactGraph.convertNodes(this.graph),
+      edges: ReactGraph.convertEdges(this.graph.edges())
     });
   }
 
@@ -50,17 +128,19 @@ class ReactGraph extends Component {
 
     return (
       <React.Fragment>
-        <Graph
-          width={900}
-          height={600}
+        <button onClick={() => this.addNode()}>Add Node</button>
+        <MyGraph
+          width={1200}
+          height={900}
           json={{
             nodes,
             edges,
-            isStatic: true,
+            isStatic: false,
             isVertical: true,
             isDirected: true
           }}
           onChange={newGraphJSON => {}}
+          Node={MyNode}
         />
       </React.Fragment>
     );
